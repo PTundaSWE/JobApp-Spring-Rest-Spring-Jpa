16fee29ea53babc5dc1d003b665312001414074f
PTundaSWE <watulukapaul@gmail.com>
2026-01-15 15:43:17 -0500

    add the rest of the data to the Job_Post table

--- src/main/java/org/paul/springbootlearning/jobappspringrestspringjpa/JobAppSpringRestSpringJpaApplication.java
+++ src/main/java/org/paul/springbootlearning/jobappspringrestspringjpa/JobAppSpringRestSpringJpaApplication.java
@@ -59,7 +59,12 @@ public class JobAppSpringRestSpringJpaApplication {
         jobPost6.setReqExperience(4);
         jobPost6.setPostTechStack(List.of("DevOps", "CI/CD", "Docker", "Kubernetes"));
 
-        repo.save(jobPost1);
+        // repo.save(jobPost1);
+        repo.save(jobPost2);
+        repo.save(jobPost3);
+        repo.save(jobPost4);
+        repo.save(jobPost5);
+        repo.save(jobPost6);
 
     }
 
