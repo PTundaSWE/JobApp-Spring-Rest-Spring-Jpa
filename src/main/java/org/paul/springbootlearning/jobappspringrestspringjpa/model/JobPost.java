27add15ada1775b272d9c3d72af915b243e09db1
PTundaSWE <watulukapaul@gmail.com>
2026-01-15 15:06:33 -0500

    feat: make JobPost a Spring JPA Entity

--- src/main/java/org/paul/springbootlearning/jobappspringrestspringjpa/model/JobPost.java
+++ src/main/java/org/paul/springbootlearning/jobappspringrestspringjpa/model/JobPost.java
@@ -1,6 +1,9 @@
 package org.paul.springbootlearning.jobappspringrestspringjpa.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,7 +14,10 @@ import java.util.List;
 @AllArgsConstructor
 @ToString
 @Component
+@Scope("prototype")
+@Entity
 public class JobPost {
+    @Id
     private int postId;
     private String postProfile;
     private String postDesc;
